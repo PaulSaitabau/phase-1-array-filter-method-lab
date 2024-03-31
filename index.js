3bfb40ae3a7784dc7d0f73207d7b43eb49c19cd4
paulsaitabau <paulsaitabau05@gmail.com>
2024-03-31 23:49:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(source, sought) {
+    return source.filter(
+       (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+   }
+   
+   //  performs fuzzy matching in a source array
+   function fuzzyMatch(source, testString) {
+    return source.filter(
+       (possibleMatch) =>
+         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+   }
+   
+   // match elements in a source array by a specific property (name)
+   function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+   }
+  
